73158e8eb796b4ede3466db3561118417da3d5b4
Fahim00153759 <khaledsaifullafahim@gmail.com>
2025-06-01 23:39:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from config import API_URL
 def main():
     downloader = FacebookVideoDownloader(API_URL)
 
-    video_url = input("https://www.facebook.com/reel/")
+    video_url = input("https://www.facebook.com/your_video_url")
     downloader.download_video(video_url)
 
 if __name__ == "__main__":
